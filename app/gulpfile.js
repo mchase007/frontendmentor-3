eb0b27ffaec7e8b343321a6dac856af84e6c8c13
unknown <>
2020-08-18 18:55:38 +0100

    js tooling setup

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,42 +1,54 @@
-const {src, dest, watch, series, parallel} = require('gulp');
+const {
+  src, dest, watch, series, parallel,
+} = require('gulp');
 const sourceMaps = require('gulp-sourcemaps');
 const scss = require('gulp-sass');
 const postCss = require('gulp-postcss');
 const autoPrefixer = require('autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
-
 
 const files = {
   htmlPath: './src/*.html',
   scssPath: './src/scss/**/*.scss',
+  jsPath: './src/js/**/*.js',
 };
 
 function scssTaskRunner() {
   return src(files.scssPath)
     .pipe(sourceMaps.init())
     .pipe(scss())
-    .pipe(postCss( [ autoPrefixer() ] ))
+    .pipe(postCss([autoPrefixer()]))
     .pipe(sourceMaps.write('.'))
-    .pipe(dest('dist/css'))
+    .pipe(dest('dist/css'));
+}
+
+// setup js taskrunner.
+function jsTaskRunner() {
+  return src(files.jsPath) // locate js files
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(uglify()) // minify main.js
+    .pipe(dest('dist')); // save to dist folder
 }
 
 const cbString = new Date().getTime();
 
 function cacheBustTaskRunner() {
   return src(files.htmlPath)
-    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-    .pipe(dest('dist'))
+    .pipe(replace(/cb=\d+/g, `cb=${cbString}`))
+    .pipe(dest('dist'));
 }
 
 function watchTaskRunner() {
   watch(
-    files.scssPath,
-    parallel(scssTaskRunner,cacheBustTaskRunner)
-  )
+    [files.scssPath, files.jsPath],
+    parallel(scssTaskRunner, jsTaskRunner, cacheBustTaskRunner),
+  );
 }
 
 exports.default = series(
-  scssTaskRunner,
+  parallel(scssTaskRunner, jsTaskRunner, cacheBustTaskRunner),
   cacheBustTaskRunner,
-  watchTaskRunner
-)
+  watchTaskRunner,
+);
