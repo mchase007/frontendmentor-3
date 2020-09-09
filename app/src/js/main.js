603f2c384f6431091760df396423930e85ed25d9
mchase <>
2020-09-09 09:16:35 +0100

    share button bug fixed

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,29 +3,33 @@ const activeContainer = document.querySelector('.active-container');
 const userDetails = document.getElementById('user');
 const alternativeShare = document.querySelector('.active .share-button');
 
-function viewShareBar() {
-  console.log('clicked');
-
-  if (window.matchMedia('(max-width: 999px)').matches) {
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 999 && activeContainer.style.display === 'flex' && userDetails.style.display === 'flex') {
     userDetails.style.display = 'none';
     activeContainer.style.display = 'flex';
-  } else {
+  } else if (window.innerWidth > 999 && activeContainer.style.display === 'flex' && userDetails.style.display === 'none') {
     userDetails.style.display = 'flex';
     activeContainer.style.display = 'flex';
-    shareButton.addEventListener('click', () => {
-      if (activeContainer.style.display === 'flex') {
-        activeContainer.style.display = 'none';
-      } else {
-        activeContainer.style.display = 'flex';
-      }
-    });
   }
-}
+});
 
 function hideShareBar() {
-  console.log('clicked');
   userDetails.style.display = 'flex';
   activeContainer.style.display = 'none';
+}
+
+function viewShareBar() {
+  if (window.innerWidth <= 999) {
+    userDetails.style.display = 'none';
+    activeContainer.style.display = 'flex';
+  } else if (window.innerWidth > 999) {
+    if (activeContainer.style.display == 'none') {
+      userDetails.style.display = 'flex';
+      activeContainer.style.display = 'flex';
+    } else if (activeContainer.style.display == 'flex') {
+      activeContainer.style.display = 'none';
+    }
+  }
 }
 
 shareButton.addEventListener('click', viewShareBar);
