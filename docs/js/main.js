e94375d69f9926a30c4b6a54ee4470f2a86de6de
mchase <>
2020-09-09 09:28:07 +0100

    update push to live version

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,36 @@
-"use strict";var shareButton=document.getElementById("share-button"),activeContainer=document.querySelector(".active-container"),userDetails=document.getElementById("user"),alternativeShare=document.querySelector(".active .share-button");function viewShareBar(){console.log("clicked"),window.matchMedia("(max-width: 999px)").matches?(userDetails.style.display="none",activeContainer.style.display="flex"):(userDetails.style.display="flex",activeContainer.style.display="flex",shareButton.addEventListener("click",function(){"flex"===activeContainer.style.display?activeContainer.style.display="none":activeContainer.style.display="flex"}))}function hideShareBar(){console.log("clicked"),userDetails.style.display="flex",activeContainer.style.display="none"}shareButton.addEventListener("click",viewShareBar),alternativeShare.addEventListener("click",hideShareBar);
+const shareButton = document.getElementById('share-button');
+const activeContainer = document.querySelector('.active-container');
+const userDetails = document.getElementById('user');
+const alternativeShare = document.querySelector('.active .share-button');
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 999 && activeContainer.style.display === 'flex' && userDetails.style.display === 'flex') {
+    userDetails.style.display = 'none';
+    activeContainer.style.display = 'flex';
+  } else if (window.innerWidth > 999 && activeContainer.style.display === 'flex' && userDetails.style.display === 'none') {
+    userDetails.style.display = 'flex';
+    activeContainer.style.display = 'flex';
+  }
+});
+
+function hideShareBar() {
+  userDetails.style.display = 'flex';
+  activeContainer.style.display = 'none';
+}
+
+function viewShareBar() {
+  if (window.innerWidth <= 999) {
+    userDetails.style.display = 'none';
+    activeContainer.style.display = 'flex';
+  } else if (window.innerWidth > 999) {
+    if (activeContainer.style.display === 'none') {
+      userDetails.style.display = 'flex';
+      activeContainer.style.display = 'flex';
+    } else if (activeContainer.style.display === 'flex') {
+      activeContainer.style.display = 'none';
+    }
+  }
+}
+
+shareButton.addEventListener('click', viewShareBar);
+alternativeShare.addEventListener('click', hideShareBar);
