079647874417632b31ae0648adb5e7d4ea158659
mchase <>
2020-09-13 00:59:01 +0100

    update

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -23,12 +23,8 @@ function viewShareBar() {
     userDetails.style.display = 'none';
     activeContainer.style.display = 'flex';
   } else if (window.innerWidth > 999) {
-    if (activeContainer.style.display === 'none') {
-      userDetails.style.display = 'flex';
-      activeContainer.style.display = 'flex';
-    } else if (activeContainer.style.display === 'flex') {
-      activeContainer.style.display = 'none';
-    }
+    userDetails.style.display = 'flex';
+    activeContainer.style.display = 'flex';
   }
 }
 
